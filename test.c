a622a8c9b8c70b764d7a735e7eefe4d823d4a067
CHEIKH <chmouhammed260594@gmail.com>
2022-08-16 17:17:47 +0000

    ajout dun code de verification

---
+++ test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main ()
+{
+int a;
+ printf("veiller saisir un nombre\n ");
+ scanf("%d ", &a);
+  if(a>0)
+  {
+    printf("votre nombre est positif \n");
+  }
+else {
+ printf(" votre nombre est negatif\n"); 
+}
+
+
+
+
+
+
+}
+
+
